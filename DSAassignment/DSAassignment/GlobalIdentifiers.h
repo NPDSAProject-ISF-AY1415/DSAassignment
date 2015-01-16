f46b13bbef3fe29d47cbdd62623029f94e27881b
itachi1706 <kennethsohyq@hotmail.com>
2015-01-16 18:14:16 +0800

    Added le comments

--- DSAassignment/DSAassignment/GlobalIdentifiers.h
+++ DSAassignment/DSAassignment/GlobalIdentifiers.h
@@ -1,6 +1,10 @@
 #ifndef GLOBAL_VARS
 #define GLOBAL_VARS
 
+/*
+This is a header that will store all the global final variables
+*/
+
 #define LYRIC_DATASET_LENGTH 210519
 #define SONG_DATASET_LENGTH 779070
 #define WORD_DATASET_LENGTH 5000
