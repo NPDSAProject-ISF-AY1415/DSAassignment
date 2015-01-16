8e405d69b4e5d60d579a388123049715cab2eb59
itachi1706 <kennethsohyq@hotmail.com>
2015-01-16 10:30:13 +0800

    Finished Merging Source Code from TestReadTextFile
    Updated to reflect code as of this commit
    
    https://github.com/NPDSAProject-ISF-AY1415/TestReadTextFile/commit/badeda9ff67a9fe0018c8c8a4447497f9fba967e

--- DSAassignment/DSAassignment/Main.cpp
+++ DSAassignment/DSAassignment/Main.cpp
@@ -1,27 +1,36 @@
-#include"Utility.h"
+#include <Windows.h>	//Console Utility
+#include <iostream>
+#include "Utility.h"
+#include "PtrArr.h"
+#include "Memory.h"
+
+using namespace std;
 using namespace utility;
 using namespace eku;
-using namespace std;
-void mainMenu();
 
-void mainMenu()
-{
+/*
+Prints out the main menu
+*/
+void mainMenu(){
 	printSeperator();
-	cout << red << "                    Main Menu (Select a storage type)" << endl;
+	cout << red << "                      Main Menu (Select a storage type)" << endl;
 	printSeperator();
 	settextcolor(white);
 	cout << "1) " << yellow << "Unsorted Pointer-Based List" << white << endl;
 	cout << "2) " << yellow << "Sorted Pointer-Based List" << white << endl;
 	cout << "3) " << yellow << "Unsorted Array-Based List" << white << endl;
 	cout << "4) " << yellow << "Sorted Array-Based List" << white << endl;
-	cout << "5) " << yellow << "View Performance Statistics" << white << endl;
+	cout << "5) " << yellow << "Performance Utilities" << white << endl;
 	cout << "6) " << yellow << "Plot Performance Graphs" << white << endl;
 	cout << "0) " << yellow << "Quit" << white << endl;
-
 }
 
-int main()
-{
+
+/*
+Main Method
+@return End Error Code
+*/
+int main(){
 	//Initialization
 	SetConsoleTitle(TEXT("DSA Read File Test Project"));
 	concolinit();
@@ -37,9 +46,9 @@ int main()
 		if (is_number(selection)){
 			switch (stoi(selection))
 			{
-			case 1: 
-			case 2: 
-			case 3: 
+			case 1:
+			case 2:
+			case 3:
 			case 0: return 0;
 				//case 4: mainList.print(); break;
 			default: cout << dark_red << "Invalid Selection." << endl; break;
